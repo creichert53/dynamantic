ccc912c66a5d005655988747def31e495a9849ab
chrisreichert53 <creichert.53@gmail.com>
2023-11-13 00:32:45 -0500

    Add Transaction and Batch to default import.

--- src/dynamantic/__init__.py
+++ src/dynamantic/__init__.py
@@ -1,3 +1,5 @@
 from .main import Dynamantic, Expr
 from .attrs import A, K
 from .indexes import GlobalSecondaryIndex, LocalSecondaryIndex
+from .transactions import TransactGet, TransactWrite
+from .batch import BatchGet, BatchWrite
